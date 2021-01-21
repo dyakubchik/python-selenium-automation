ab44719bb74b0a826ebadf900fefa5589d061ea7
dyakubchik <dmitry.yakubchik@gmail.com>
2021-01-21 11:35:22 -0500

    Home Work commit

--- features/environment.py
+++ features/environment.py
@@ -5,7 +5,7 @@ def browser_init(context):
     """
     :param context: Behave context
     """
-    context.driver = webdriver.Chrome()
+    context.driver = webdriver.Chrome(executable_path=r'C:\Users\dmitr\OneDrive\Education\Automation Testing\Bootcamp - JobEasy\GitHub_Repository\python-selenium-automation\chromedriver.exe')
     # context.browser = webdriver.Safari()
     # context.browser = webdriver.Firefox()
 
--- features/tests/product_search.feature
+++ features/tests/product_search.feature
@@ -1,4 +1,4 @@
-# Created by Svetlana at 4/4/19
+# Created by Svetlana at 4/4/19, changed by Dmitry 1/21/2021
 Feature: Test Scenarios for Search functionality
 
   Scenario: User can search for a product

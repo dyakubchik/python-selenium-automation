14825eb92544ce5ba1e3e5314acddb4362d2a3db
dyakubchik <dmitry.yakubchik@gmail.com>
2021-02-09 20:41:53 -0500

    add class work

--- features/steps/amazon_search.py
+++ features/steps/amazon_search.py
@@ -7,10 +7,10 @@ def open_amazon(context):
     context.driver.get('https://www.amazon.com/')
 
 
-@when('Input Watches into Amazon search field')
-def input_amazon_search(context):
+@when('Input {search_query} into Amazon search field')
+def input_amazon_search(context, search_query):
     search_field = context.driver.find_element(By.ID, 'twotabsearchtextbox')
-    search_field.send_keys('Watches')
+    search_field.send_keys(search_query)
 
 
 @when('Click on Amazon search icon')
@@ -18,13 +18,13 @@ def click_search_amazon(context):
     context.driver.find_element(By.ID, 'nav-search-submit-button').click()
 
 
-@then('Product results for Watches are shown on Amazon')
-def verify_search_result(context):
+@then('Product results for {results_word} are shown on Amazon')
+def verify_search_result(context, results_word):
     actual_text = context.driver.find_element(By.XPATH, "//span[@class='a-color-state a-text-bold']").text
-    expected_text = '"Watches"'
+    expected_text = f'{results_word}'
     assert expected_text == actual_text, f'Expected {expected_text}, but got {actual_text}'\
 
 
-@then('Page URL has Watches in it')
-def verify_url_contains_query(context):
-    assert 'Watches' in context.driver.current_url, f'Watches not in {context.driver.current_url}'
+@then('Page URL has {query} in it')
+def verify_url_contains_query(context, query):
+    assert query in context.driver.current_url, f'Query not in {context.driver.current_url}'
--- features/tests/amazon_search.feature
+++ features/tests/amazon_search.feature
@@ -2,9 +2,17 @@
 Feature: Amazon Search Test
   # Enter feature description here
 
-  Scenario: User can search for a product
+  Scenario: User can search for a Watches
     Given Open Amazon page
     When Input Watches into Amazon search field
     And Click on Amazon search icon
-    Then Product results for Watches are shown on Amazon
+    Then Product results for "Watches" are shown on Amazon
     And Page URL has Watches in it
+
+  Scenario: User can search for a Dress
+    Given Open Amazon page
+    When Input Dress into Amazon search field
+    And Click on Amazon search icon
+    Then Product results for "Dress" are shown on Amazon
+    And Page URL has Dress in it
+
